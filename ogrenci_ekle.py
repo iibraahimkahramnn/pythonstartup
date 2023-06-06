399e411685f349abaedc8934de8c431cbdb20992
ibrahmjk1903 <110205635+ibrahmjk1903@users.noreply.github.com>
2023-06-06 14:23:19 +0300

    deneme7

--- ogrenci_ekle.py
+++ ogrenci_ekle.py
@@ -17,41 +17,35 @@ def student_add():
     
     
 
-    def last_name (lastname):
-        if lastname.isalpha():
-            print(" ")
-        else:
-            print("Lütfen sadece harf giriniz.")
-            
-    input_str = input("Öğrencinin soyad'ını girin: ")
-    last_name(input_str)
-
-
-    def age (yas):
-        if yas.isalpha():
-            print(" ")
-        else:
-            print("Lütfen sadece sayı giriniz.")
-            
-    input_int = input("Öğrencinin yaş'ını girin: ")
-    first_name(input_int)
-
-
-    def gender (cinsiyet):
-        if cinsiyet.isalpha():
-            print(" ")
-        else:
-            print("Lütfen sadece harf giriniz.")
-            
-    input_str = input("Öğrenci'nin cinsiyet'ini girin: ")
-    first_name(input_str)
+    last_name = input("Öğrencinin soyad'ını girin: ")
+    if last_name.isalpha():
+        print(" ")
+    else:
+        print("Lütfen sadece harf giriniz.")
+    last_name()
 
 
 
+
+    age= input("Öğrencinin yaş'ını girin: ")
+    if age.isalpha():
+        print(" ")
+    else:
+        print("Lütfen sadece sayı giriniz.")
+    age()      
+    
+
+    gender = input("Öğrenci'nin cinsiyet'ini girin: ")
+    if gender.isalpha():
+        print(" ")
+    else:
+        print("Lütfen sadece harf giriniz.")
+    gender()
+    
 
     cursor.execute(
         "INSERT INTO students (first_name, last_name, age, gender) VALUES (?, ?, ?, ?)",
-        (first_name, last_name, age, gender)
+        (name, last_name, age, gender)
     )
 
     conn.commit()  # Değişiklikleri veritabanına kaydet
