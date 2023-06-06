d86f6ea5194654eafe261377a8c6610a74324c3f
ibrahmjk1903 <110205635+ibrahmjk1903@users.noreply.github.com>
2023-06-06 14:06:50 +0300

    deneme2

--- ogrenci_ekle.py
+++ ogrenci_ekle.py
@@ -5,10 +5,47 @@ cursor = conn.cursor()
 
 
 def student_add():
-    first_name = input("Öğrenci adı giriniz: ")
-    last_name = input("Öğrenci'nin soyadını giriniz: ")
-    age = input("Öğrenci'nin yaşını giriniz: ")
-    gender = input("Öğrenci'nin cinsiyetini giriniz: ")
+
+    def first_name (name):
+        if name.isalpha():
+            print(" ")
+        else:
+            print("Lütfen sadece harf giriniz.")
+
+    input_str = input("Öğrencinin ismini girin: ")
+    first_name(input_str)
+
+    def last_name (lastname):
+        if lastname.isalpha():
+            print(" ")
+        else:
+            print("Lütfen sadece harf giriniz.")
+            
+    input_str = input("Öğrencinin soyad'ını girin: ")
+    last_name(input_str)
+
+
+    def age (yas):
+        if yas.isalpha():
+            print(" ")
+        else:
+            print("Lütfen sadece sayı giriniz.")
+            
+    input_int = input("Öğrencinin yaş'ını girin: ")
+    first_name(input_int)
+
+
+    def gender (cinsiyet):
+        if cinsiyet.isalpha():
+            print(" ")
+        else:
+            print("Lütfen sadece harf giriniz.")
+            
+    input_str = input("Öğrenci'nin cinsiyet'ini girin: ")
+    first_name(input_str)
+
+
+
 
     cursor.execute(
         "INSERT INTO students (first_name, last_name, age, gender) VALUES (?, ?, ?, ?)",
